e3d486ec9ee95d463b3d59e34d0750767d00b629
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-04-25 11:17:03 +0300

    Initial Commit

---
+++ Schedule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Schedule
+//
+//  Created by Artem Kutasevich on 25.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Schedule/ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleApp.swift
+//  Schedule
+//
+//  Created by Artem Kutasevich on 25.04.22.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
